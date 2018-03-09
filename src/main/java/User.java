8e64d10488d715d2af0d86a5faf77c969a2bf76d
xinghao1998 <773729229@qq.com>
2018-03-09 15:13:36 +0800

    My First Commit

---
+++ src/main/java/User.java
@@ -0,0 +1,15 @@
+public class User {
+
+    private Integer id;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
