17cdcf9f52e73b1ff49baca7b6b505f5f1b08533
xinghao1998 <773729229@qq.com>
2018-03-09 16:04:27 +0800

    add a file name

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,7 @@
 public class User {
 
     private Integer id;
+    private String name;
 
     public User() {
     }
@@ -11,5 +12,13 @@ public class User {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
